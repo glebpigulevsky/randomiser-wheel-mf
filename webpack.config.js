3b0ed883ac5edf2032f92d8be841e4bb2575d5ad
gleb <gleb.pigulevsky@hqo.co>
2022-09-04 16:10:07 +0400

    fix: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
+var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
+var server_host = process.env.YOUR_HOST || "0.0.0.0";
+
 const deps = require("./package.json").dependencies;
 module.exports = {
   output: {
@@ -12,7 +15,8 @@ module.exports = {
   },
 
   devServer: {
-    port: 3000,
+    port: server_port,
+    host: server_host,
     historyApiFallback: true,
   },
 
